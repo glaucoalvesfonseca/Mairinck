49858b032e20074260afbfb79ce194aca770fd68
Paulo Henrique Vasconcellos <phsamus@gmail.com>
2017-05-09 10:49:15 -0300

    Finalização da biblioteca de e-mails; arquivo de teste apagado

--- enviar_email.py
+++ enviar_email.py
@@ -3,31 +3,44 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-def enviar_email(assunto, remetente, destinatario, mensagem, anexo=False):
+def enviar_email(assunto, remetente, destinatario, mensagem, anexo=False, nome_anexo='Anexo'):
+    """
+    Função para envio de e-mails com ou sem anexo.
     
-    #Enviando o email 
+    A função enviar_email utiliza seis parâmetros e, após passados pelo usuário, a função montará o e-mail e enviará para o destinatário. As bibliotecas, MIMEBase e encoders só serão importadas se o argumento anexo tiver um valor diferente de False.
+
+    Argumentos:
+    
+    Assunto: string. Assunto a ser enviado no e-mail;
+    Remetente: string. Nome do remetente. Não precisa ser o e-mail;
+    Destinatário: string. E-mail do destinatário;
+    Mensagem: string. Mensagem a ser enviada no corpo do e-mail;
+    Anexo: string. Caminho para acesso ao arquivo que será enviado em anexo;
+    Nome_anexo: string. O usuário pode especificar um nome personalizado para o anexo. Por padrão, o nome será Anexo.
+
+    """
+    #Preparando dados do e-mail
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login('arobotwithoutsarcasm@gmail.com', "machineonetwo")
     
-    msg = MIMEText(mensagem)
+    msg = MIMEMultipart()
     msg['Subject'] = assunto
     msg['FROM'] = remetente
     msg['To'] = destinatario
 
     #Verifica se deseja enviar anexo
     if anexo:
-        msg.add_header('Content-Disposition', 'attachment', filename=anexo)
-        msg.attach(MIMEMultipart(anexo))
+        from email.mime.base import MIMEBase
+        from email import encoders
+        
+        msg.attach(MIMEText(mensagem))
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload(open(anexo, "rb").read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="{}"'.format(nome_anexo))
+        msg.attach(part)
 
     #Enviando e-mail
     server.sendmail('TARS', destinatario, msg.as_string().encode('ascii'))
     server.quit()
-
-assunto = 'Teste'
-remetente = 'TARS'
-destinatario = 'pvasconcellos@araruama.unimed.com.br'
-mensagem = 'Mensagem de teste'
-anexo = os.path.abspath(os.path.dirname(__file__) + '\\teste.txt')
-
-enviar_email(assunto, remetente, destinatario, mensagem, anexo=False)
